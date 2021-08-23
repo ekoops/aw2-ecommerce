863626bb4a14b4d0a50ff511cb9a4e020aa74d4d
Renato Ambrosone <s279186@studenti.polito.it>
2021-08-23 17:32:06 +0200

    eureka

--- wallet-service/src/main/kotlin/it/aw2commerce/walletservice/WalletServiceApplication.kt
+++ wallet-service/src/main/kotlin/it/aw2commerce/walletservice/WalletServiceApplication.kt
@@ -2,9 +2,11 @@ package it.aw2commerce.walletservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 
 @SpringBootApplication
+@EnableEurekaClient
 class WalletServiceApplication
 
 fun main(args: Array<String>) {
