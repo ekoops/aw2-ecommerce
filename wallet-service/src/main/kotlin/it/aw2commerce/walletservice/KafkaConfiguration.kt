09c5357c608757dde3508af0429dc2969c61ecfe
ekoops <leonardodigiovanna1@gmail.com>
2021-08-27 22:53:55 +0200

    Added consumers and producers infrastructure

--- wallet-service/src/main/kotlin/it/aw2commerce/walletservice/KafkaConfiguration.kt
+++ wallet-service/src/main/kotlin/it/aw2commerce/walletservice/KafkaConfiguration.kt
@@ -1,11 +1,16 @@
 package it.aw2commerce.walletservice
 
 import it.aw2commerce.walletservice.dto.incoming.CreateWalletRequestDTO
+import it.aw2commerce.walletservice.dto.kafka.BudgetAvailabilityProducedDTO
+import it.aw2commerce.walletservice.dto.kafka.BudgetAvailabilityRequestDTO
+import it.aw2commerce.walletservice.dto.kafka.OrderApprovedByWalletDTO
+import it.aw2commerce.walletservice.dto.kafka.OrderDTO
 import org.apache.kafka.clients.admin.NewTopic
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
@@ -15,48 +20,110 @@ import org.springframework.kafka.support.serializer.JsonDeserializer
 import org.springframework.kafka.support.serializer.JsonSerializer
 
 @Configuration
-class KafkaConfiguration {
-
+class KafkaConfiguration(
+    @Value("\${spring.kafka.bootstrap-servers}") private val bootstrapServers: String
+) {
+    // CONSUMER for topic budget-availability-requested
     @Bean
-    fun createWalletRequestDTOProducerFactory(): ProducerFactory<String , CreateWalletRequestDTO>{
-        val configProps = mutableMapOf<String , Any>()
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG , "localhost:9092")
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG , StringSerializer::class.java)
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG , JsonSerializer::class.java)
-        return DefaultKafkaProducerFactory(configProps)
+    fun budgetAvailabilityRequestedTopic(): NewTopic {
+        return TopicBuilder.name("budget-availability-requested").build()
     }
-
     @Bean
-    fun createWalletRequestDTOKafkaTemplate(): KafkaTemplate<String, CreateWalletRequestDTO>{
-        return KafkaTemplate(createWalletRequestDTOProducerFactory())
-    }
-
-    @Bean
-    fun createWalletRequestDTOConsumerFactory(): ConsumerFactory<String , CreateWalletRequestDTO> {
-        val configProps = mutableMapOf<String , Any>()
-        configProps[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = "localhost:9092"
+    fun budgetAvailabilityRequestedConsumerFactory(): ConsumerFactory<String, OrderDTO> {
+        val configProps = mutableMapOf<String, Any>()
+        configProps[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = this.bootstrapServers
         configProps[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
         configProps[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = JsonDeserializer::class.java
-        configProps[JsonDeserializer.VALUE_DEFAULT_TYPE] = CreateWalletRequestDTO::class.java
+        configProps[JsonDeserializer.VALUE_DEFAULT_TYPE] = OrderDTO::class.java
         return DefaultKafkaConsumerFactory(configProps)
     }
-
     @Bean
-    fun createWalletRequestDTOContainerFactory(): ConcurrentKafkaListenerContainerFactory<String , CreateWalletRequestDTO>{
-        val factory = ConcurrentKafkaListenerContainerFactory<String , CreateWalletRequestDTO>()
-        factory.consumerFactory = createWalletRequestDTOConsumerFactory()
+    fun budgetAvailabilityRequestedContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, OrderDTO> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, OrderDTO>()
+        factory.consumerFactory = budgetAvailabilityRequestedConsumerFactory()
+        return factory
+    }
+
+    // CONSUMER for topic order-approved
+    @Bean
+    fun orderApprovedTopic(): NewTopic {
+        return TopicBuilder.name("order-approved").build()
+    }
+     @Bean
+    fun orderApprovedConsumerFactory(): ConsumerFactory<String, OrderDTO> {
+        val configProps = mutableMapOf<String, Any>()
+        configProps[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = this.bootstrapServers
+        configProps[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        configProps[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = JsonDeserializer::class.java
+        configProps[JsonDeserializer.VALUE_DEFAULT_TYPE] = OrderDTO::class.java
+        return DefaultKafkaConsumerFactory(configProps)
+    }
+    @Bean
+    fun orderApprovedContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, OrderDTO> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, OrderDTO>()
+        factory.consumerFactory = orderApprovedConsumerFactory()
         return factory
     }
 
 
-//    @Bean
-//    fun topic1(): NewTopic{
-//        return TopicBuilder.name("prova")
-//            .replicas(2)
-//            .build()
-//    }
+    // CONSUMER for topic order-cancelled
+    @Bean
+    fun orderCancelledTopic(): NewTopic {
+        return TopicBuilder.name("order-cancelled").build()
+    }
+     @Bean
+    fun orderCancelledConsumerFactory(): ConsumerFactory<String, OrderDTO> {
+        val configProps = mutableMapOf<String, Any>()
+        configProps[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = this.bootstrapServers
+        configProps[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        configProps[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = JsonDeserializer::class.java
+        configProps[JsonDeserializer.VALUE_DEFAULT_TYPE] = OrderDTO::class.java
+        return DefaultKafkaConsumerFactory(configProps)
+    }
+    @Bean
+    fun orderCancelledContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, OrderDTO> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, OrderDTO>()
+        factory.consumerFactory = orderCancelledConsumerFactory()
+        return factory
+    }
 
 
+    // PRODUCER for topic budget-availability-produced
+    @Bean
+    fun budgetAvailabilityProducedTopic(): NewTopic {
+        return TopicBuilder.name("budget-availability-produced").build()
+    }
+    @Bean
+    fun budgetAvailabilityProducedProducerFactory(): ProducerFactory<String, BudgetAvailabilityProducedDTO> {
+        val configProps = mutableMapOf<String, Any>()
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, this.bootstrapServers)
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java)
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer::class.java)
+        return DefaultKafkaProducerFactory(configProps)
+    }
+
+    @Bean
+    fun budgetAvailabilityProducedKafkaTemplate(): KafkaTemplate<String, BudgetAvailabilityProducedDTO> {
+        return KafkaTemplate(budgetAvailabilityProducedProducerFactory())
+    }
 
 
+    // PRODUCER for topic order-approved-by-wallet
+    @Bean
+    fun orderApprovedByWalletTopic(): NewTopic {
+        return TopicBuilder.name("order-approved-by-wallet").build()
+    }
+    @Bean
+    fun orderApprovedByWalletProducerFactory(): ProducerFactory<String, OrderApprovedByWalletDTO> {
+        val configProps = mutableMapOf<String, Any>()
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, this.bootstrapServers)
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java)
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer::class.java)
+        return DefaultKafkaProducerFactory(configProps)
+    }
+
+    @Bean
+    fun orderApprovedByWalletKafkaTemplate(): KafkaTemplate<String, OrderApprovedByWalletDTO> {
+        return KafkaTemplate(orderApprovedByWalletProducerFactory())
+    }
 }
--- wallet-service/src/main/kotlin/it/aw2commerce/walletservice/listeners/RequestListener.kt
+++ wallet-service/src/main/kotlin/it/aw2commerce/walletservice/listeners/RequestListener.kt
@@ -1,20 +1,89 @@
 package it.aw2commerce.walletservice.listeners
 
+import it.aw2commerce.walletservice.domain.Transaction
 import it.aw2commerce.walletservice.dto.incoming.CreateWalletRequestDTO
+import it.aw2commerce.walletservice.dto.kafka.BudgetAvailabilityProducedDTO
+import it.aw2commerce.walletservice.dto.kafka.OrderApprovedByWalletDTO
+import it.aw2commerce.walletservice.dto.kafka.OrderDTO
+import it.aw2commerce.walletservice.repositories.WalletRepository
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
 
 
 @Component
-class RequestListener {
+class RequestListener(
+    private val budgetAvailabilityProducedKafkaTemplate: KafkaTemplate<String, BudgetAvailabilityProducedDTO>,
+    private val orderApprovedByWalletKafkaTemplate: KafkaTemplate<String, OrderApprovedByWalletDTO>,
+    private val walletRepository: WalletRepository
+) {
 
-//    @KafkaListener(
-//        id = "walletService" ,
-//        topics = ["prova"],
-//        containerFactory = "createWalletRequestDTOContainerFactory"
-//    )
-//    fun listen(createWalletRequestDTO: CreateWalletRequestDTO){
-//        //TODO
-//    }
+    @KafkaListener(
+        id = "wallet-svc-grp",
+        topics = ["budget-availability-requested"],
+        containerFactory = "budgetAvailabilityRequestedContainerFactory",
+
+        )
+    fun listenBudgetAvailabilityRequested(key: String, orderDTO: OrderDTO) {
+        val amount = orderDTO.items.fold(0.0) { acc, orderItemDTO ->
+            acc + orderItemDTO.amount * orderItemDTO.perItemPrice
+        }
+        // check budget availability
+        //  this.walletRepository...
+        val isBudgetAvailable = true // TODO mocked
+        val budgetAvailabilityProducedDTO = if (isBudgetAvailable)
+            BudgetAvailabilityProducedDTO(
+                ok = orderDTO
+            ) else
+            BudgetAvailabilityProducedDTO(
+                failure = "no budget" // TODO: change error description
+            )
+        budgetAvailabilityProducedKafkaTemplate.send(
+            "budget-availability-produced",
+            key,
+            budgetAvailabilityProducedDTO
+        ).get()
+        // TODO handle possibile kafka exception
+    }
+
+    @KafkaListener(
+        id = "wallet-svc-grp",
+        topics = ["order-approved"],
+        containerFactory = "orderApprovedContainerFactory"
+    )
+    fun listenOrderApproved(key: String, orderDTO: OrderDTO) {
+        //TODO
+        val wallet = this.walletRepository.getWalletByCustomerId(orderDTO.buyerId.toLong())
+        if (wallet == null) {
+            val orderApprovedByWalletDTO = OrderApprovedByWalletDTO(
+                failure = "no wallet"
+            )
+            orderApprovedByWalletKafkaTemplate.send(
+                "order-approved-by-wallet",
+                key,
+                orderApprovedByWalletDTO
+            ).get()
+            return
+        }
+        val amount = orderDTO.items.fold(0.0) { acc, orderItemDTO ->
+            acc + orderItemDTO.amount * orderItemDTO.perItemPrice
+        }
+//        val transaction = Transaction(
+//            purchasingWallet = wallet,
+//            rechargingWallet = wallet, // TODO who has to be recharged???,
+//            amount = (amount*100).toLong(),
+//            timeInstant = // TODO???,
+//        )
+//        this.walletRepository...
+    }
+
+    @KafkaListener(
+        id = "wallet-svc-grp",
+        topics = ["order-cancelled"],
+        containerFactory = "orderCancelledContainerFactory"
+    )
+    fun listenOrderCancelled(orderDTO: OrderDTO) {
+        //TODO
+    }
 
 }
