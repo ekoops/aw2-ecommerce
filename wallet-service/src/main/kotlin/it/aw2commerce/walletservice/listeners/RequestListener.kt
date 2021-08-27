bfb63f4eff3501e32c3ef013d62ea7b7f8906665
Renato Ambrosone <s279186@studenti.polito.it>
2021-08-28 01:05:14 +0200

    check budget availability

--- wallet-service/src/main/kotlin/it/aw2commerce/walletservice/listeners/RequestListener.kt
+++ wallet-service/src/main/kotlin/it/aw2commerce/walletservice/listeners/RequestListener.kt
@@ -1,21 +1,31 @@
 package it.aw2commerce.walletservice.listeners
 
 import it.aw2commerce.walletservice.domain.Transaction
+import it.aw2commerce.walletservice.domain.toTransactionDTO
 import it.aw2commerce.walletservice.dto.incoming.CreateWalletRequestDTO
+import it.aw2commerce.walletservice.dto.kafka.ApprovationDTO
 import it.aw2commerce.walletservice.dto.kafka.BudgetAvailabilityProducedDTO
 import it.aw2commerce.walletservice.dto.kafka.OrderApprovedByWalletDTO
 import it.aw2commerce.walletservice.dto.kafka.OrderDTO
+import it.aw2commerce.walletservice.repositories.TransactionRepository
 import it.aw2commerce.walletservice.repositories.WalletRepository
+import it.aw2commerce.walletservice.services.WalletService
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.RequestHeader
+import java.time.Instant
+import java.time.LocalDateTime
 
 
 @Component
 class RequestListener(
     private val budgetAvailabilityProducedKafkaTemplate: KafkaTemplate<String, BudgetAvailabilityProducedDTO>,
     private val orderApprovedByWalletKafkaTemplate: KafkaTemplate<String, OrderApprovedByWalletDTO>,
-    private val walletRepository: WalletRepository
+    private val walletRepository: WalletRepository,
+    private val transactionRepository: TransactionRepository
 ) {
 
     @KafkaListener(
@@ -24,13 +34,31 @@ class RequestListener(
         containerFactory = "budgetAvailabilityRequestedContainerFactory",
 
         )
-    fun listenBudgetAvailabilityRequested(key: String, orderDTO: OrderDTO) {
+    fun listenBudgetAvailabilityRequested(@RequestHeader("key") key: String, orderDTO: OrderDTO) {
         val amount = orderDTO.items.fold(0.0) { acc, orderItemDTO ->
             acc + orderItemDTO.amount * orderItemDTO.perItemPrice
         }
         // check budget availability
-        //  this.walletRepository...
-        val isBudgetAvailable = true // TODO mocked
+        val wallet = this.walletRepository.getWalletByCustomerId(orderDTO.buyerId.toLong())
+        if (wallet == null) {
+            val orderApprovedByWalletDTO = OrderApprovedByWalletDTO(
+                failure = "no wallet"
+            )
+            orderApprovedByWalletKafkaTemplate.send(
+                "order-approved-by-wallet",
+                key,
+                orderApprovedByWalletDTO
+            ).get()
+            return
+        }
+
+        val walletTransactions:Page<Transaction> =  this.transactionRepository.findAllByPurchasingWalletOrRechargingWallet(
+            purchasingWallet = wallet,
+            rechargingWallet = wallet,
+            pageable = PageRequest.of(0, TransactionRepository.TRANSACTION_PAGE_SIZE)) //todo check zero
+        val budget = walletTransactions.fold(0L) { acc, transaction -> acc + transaction.amount }
+
+        val isBudgetAvailable = budget > amount*100
         val budgetAvailabilityProducedDTO = if (isBudgetAvailable)
             BudgetAvailabilityProducedDTO(
                 ok = orderDTO
@@ -51,8 +79,8 @@ class RequestListener(
         topics = ["order-approved"],
         containerFactory = "orderApprovedContainerFactory"
     )
-    fun listenOrderApproved(key: String, orderDTO: OrderDTO) {
-        //TODO
+    fun listenOrderApproved(@RequestHeader("key") key: String, orderDTO: OrderDTO) {
+        //TODO controlla se RequestHeader va bene
         val wallet = this.walletRepository.getWalletByCustomerId(orderDTO.buyerId.toLong())
         if (wallet == null) {
             val orderApprovedByWalletDTO = OrderApprovedByWalletDTO(
@@ -68,13 +96,29 @@ class RequestListener(
         val amount = orderDTO.items.fold(0.0) { acc, orderItemDTO ->
             acc + orderItemDTO.amount * orderItemDTO.perItemPrice
         }
-//        val transaction = Transaction(
-//            purchasingWallet = wallet,
-//            rechargingWallet = wallet, // TODO who has to be recharged???,
-//            amount = (amount*100).toLong(),
-//            timeInstant = // TODO???,
-//        )
-//        this.walletRepository...
+        val transaction = Transaction(
+            purchasingWallet = wallet,
+            rechargingWallet = wallet, // TODO who has to be recharged???,
+            amount = (amount * 100).toLong(),
+            timeInstant = LocalDateTime.now(),
+        )
+        val createdTransaction = transactionRepository.save(transaction)
+
+        val orderApprovedByWalletDTO = if (createdTransaction.getId() == null) {
+            OrderApprovedByWalletDTO(
+                failure = "no wallet"
+            )
+        } else {
+            OrderApprovedByWalletDTO(
+                ok = ApprovationDTO("WALLET", orderDTO)
+            )
+
+        }
+        orderApprovedByWalletKafkaTemplate.send(
+            "order-approved-by-wallet",
+            key,
+            orderApprovedByWalletDTO
+        ).get()
     }
 
     @KafkaListener(
