f02ed59cf51800d81e36cb7b6ecf094ee57d4ba7
ekoops <leonardodigiovanna1@gmail.com>
2021-09-26 18:25:29 +0200

    updated init configuration

--- catalog-service/src/main/kotlin/it/polito/ecommerce/catalogservice/configurations/DbInitConfiguration.kt
+++ catalog-service/src/main/kotlin/it/polito/ecommerce/catalogservice/configurations/DbInitConfiguration.kt
@@ -19,52 +19,85 @@ class DbInitConfiguration(
      private val userRepository: UserRepository,
      private val customerRepository: CustomerRepository,
      private val passwordEncoder: PasswordEncoder) {
-    final val user1 = User(
-        username = "user1",
-        email = "user1@yopmail.com",
-        password = passwordEncoder.encode("password"),
-        isEnabled = true,
-        isLocked = false,
-        rolesList = listOf(Rolename.ADMIN, Rolename.CUSTOMER)
-    )
-
-    final val user2 = User(
-        username = "user2",
-        email = "user2@yopmail.com",
-        password = passwordEncoder.encode("password"),
-        isEnabled = true,
-        isLocked = false,
-        rolesList = listOf(Rolename.ADMIN)
-    )
-
-    final val user3 = User(
-        username = "user3",
-        email = "user3@yopmail.com",
-        password = passwordEncoder.encode("password"),
-        isEnabled = true,
-        isLocked = false,
-        rolesList = listOf(Rolename.CUSTOMER)
-    )
-
-    val map = mapOf<String, User>(
-        "user1" to user1,
-        "user2" to user2,
-        "user3" to user3
-    )
 
     @Bean
     fun commandLineRunner() = CommandLineRunner {
         println("@@@@@ DbInitConfiguration Started!! v3 @@@@@")
 
+        val user1 = User(
+            username = "user1",
+            email = "user1@yopmail.com",
+            password = passwordEncoder.encode("password"),
+            isEnabled = true,
+            isLocked = false,
+            rolesList = listOf(Rolename.ADMIN, Rolename.CUSTOMER)
+        )
+
+        val user2 = User(
+            username = "user2",
+            email = "user2@yopmail.com",
+            password = passwordEncoder.encode("password"),
+            isEnabled = true,
+            isLocked = false,
+            rolesList = listOf(Rolename.ADMIN)
+        )
+
+        val user3 = User(
+            username = "user3",
+            email = "user3@yopmail.com",
+            password = passwordEncoder.encode("password"),
+            isEnabled = true,
+            isLocked = false,
+            rolesList = listOf(Rolename.CUSTOMER)
+        )
 
         userRepository
             .findByUsername("user1")
-            .switchIfEmpty(userRepository.save(map[it.]!!))
+            .switchIfEmpty(userRepository.save(user1))
             .flatMap {
                 val customer = Customer(
                     name = "user1_name",
                     surname = "user1_surname",
                     deliveryAddress = "user1_deliveryAddress",
+                    user = it
+                )
+                return@flatMap customerRepository.save(customer)
+            }
+            .subscribe(
+                {
+                    println("@@@! Received item: ${it.id}")
+                },
+                {
+                    println("@@@! Received error: ${it.message}")
+                },
+                {
+                    println("@@@! Completed")
+                },
+            )
+
+        userRepository
+            .findByUsername("user2")
+            .switchIfEmpty(userRepository.save(user2))
+            .subscribe(
+                {
+                    println("@@@! Received item: ${it.id}")
+                },
+                {
+                    println("@@@! Received error: ${it.message}")
+                },
+                {
+                    println("@@@! Completed")
+                },
+            )
+
+        userRepository
+            .findByUsername("user3")
+            .switchIfEmpty(userRepository.save(user3))
+            .flatMap {
+                val customer = Customer(
+                    name = "user3_name",
+                    surname = "user3_surname",
+                    deliveryAddress = "user3_deliveryAddress",
                     user = it
                 )
                 return@flatMap customerRepository.save(customer)
