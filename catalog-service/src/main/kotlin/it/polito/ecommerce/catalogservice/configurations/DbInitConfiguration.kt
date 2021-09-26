8fb31c5d81c4599ad38c0bf91898163ae8fdecb9
crisz <crissstian96@gmail.com>
2021-09-26 18:14:36 +0200

    cose

--- catalog-service/src/main/kotlin/it/polito/ecommerce/catalogservice/configurations/DbInitConfiguration.kt
+++ catalog-service/src/main/kotlin/it/polito/ecommerce/catalogservice/configurations/DbInitConfiguration.kt
@@ -11,83 +11,74 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.crypto.password.PasswordEncoder
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @Configuration
-class DbInitConfiguration {
+class DbInitConfiguration(
+     private val userRepository: UserRepository,
+     private val customerRepository: CustomerRepository,
+     private val passwordEncoder: PasswordEncoder) {
+    final val user1 = User(
+        username = "user1",
+        email = "user1@yopmail.com",
+        password = passwordEncoder.encode("password"),
+        isEnabled = true,
+        isLocked = false,
+        rolesList = listOf(Rolename.ADMIN, Rolename.CUSTOMER)
+    )
+
+    final val user2 = User(
+        username = "user2",
+        email = "user2@yopmail.com",
+        password = passwordEncoder.encode("password"),
+        isEnabled = true,
+        isLocked = false,
+        rolesList = listOf(Rolename.ADMIN)
+    )
+
+    final val user3 = User(
+        username = "user3",
+        email = "user3@yopmail.com",
+        password = passwordEncoder.encode("password"),
+        isEnabled = true,
+        isLocked = false,
+        rolesList = listOf(Rolename.CUSTOMER)
+    )
+
+    val map = mapOf<String, User>(
+        "user1" to user1,
+        "user2" to user2,
+        "user3" to user3
+    )
+
     @Bean
-    fun commandLineRunner(
-        userRepository: UserRepository,
-        customerRepository: CustomerRepository,
-        passwordEncoder: PasswordEncoder
-    ) = CommandLineRunner {
-        userRepository.findByUsername("user1").map { throw Exception() }.subscribe(
-            {println("PROVA onNext")},
-            {println("PROVA onError")},
-            {println("PROVA onComplete")},
-        )
-//        user1.flatMap {
-//            println("USER obtained user: $it")
-//            if (it == null) {
-//                val newUser = User(
-//                    username = "user1",
-//                    email = "user1@yopmail.com",
-//                    password = passwordEncoder.encode("password"),
-//                    isEnabled = true,
-//                    isLocked = false,
-//                    rolesList = listOf(Rolename.ADMIN, Rolename.CUSTOMER)
-//                )
-//                userRepository.save(newUser).flatMap { createdUser ->
-//                    val customer = Customer(
-//                        name = "user1_name",
-//                        surname = "user1_surname",
-//                        deliveryAddress = "user1_deliveryAddress",
-//                        user = createdUser
-//                    )
-//                    customerRepository.save(customer).map { createdUser }
-//                }
-//            } else user1
-//        }.subscribe({println("USER INFO: user1 created")}, {err -> println("USER ERR: $err")})
+    fun commandLineRunner() = CommandLineRunner {
+        println("@@@@@ DbInitConfiguration Started!! v3 @@@@@")
 
-//        val user2 = userRepository.findByUsername("user2")
-//        user2.flatMap {
-//            if (it == null) {
-//                val newUser = User(
-//                    username = "user2",
-//                    email = "user2@yopmail.com",
-//                    password = passwordEncoder.encode("password"),
-//                    isEnabled = true,
-//                    isLocked = false,
-//                    rolesList = listOf(Rolename.ADMIN)
-//                )
-//                userRepository.save(newUser)
-//            } else user2
-//        }.block()
-//        println("user2 created")
-//
-//
-//        val user3 = userRepository.findByUsername("user3")
-//        user3.flatMap {
-//            if (it == null) {
-//                val newUser = User(
-//                    username = "user3",
-//                    email = "user3@yopmail.com",
-//                    password = passwordEncoder.encode("password"),
-//                    isEnabled = true,
-//                    isLocked = false,
-//                    rolesList = listOf(Rolename.CUSTOMER)
-//                )
-//                userRepository.save(newUser).flatMap { createdUser ->
-//                    val customer = Customer(
-//                        name = "user3_name",
-//                        surname = "user3_surname",
-//                        deliveryAddress = "user3_deliveryAddress",
-//                        user = createdUser
-//                    )
-//                    customerRepository.save(customer).map { createdUser }
-//                }
-//            } else user3
-//        }.block()
-//        println("user3 created")
 
+        userRepository
+            .findByUsername("user1")
+            .switchIfEmpty(userRepository.save(map[it.]!!))
+            .flatMap {
+                val customer = Customer(
+                    name = "user1_name",
+                    surname = "user1_surname",
+                    deliveryAddress = "user1_deliveryAddress",
+                    user = it
+                )
+                return@flatMap customerRepository.save(customer)
+            }
+            .subscribe(
+                {
+                    println("@@@! Received item: ${it.id}")
+                },
+                {
+                    println("@@@! Received error: ${it.message}")
+                },
+                {
+                    println("@@@! Completed")
+                },
+            )
     }
 }
